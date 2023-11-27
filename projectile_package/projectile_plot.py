42259b50e18f117903043d9b34004655ef2dff1a
Tomas Williams <tomasjwilliams@icloud.com>
2023-11-27 19:25:29 +0000

    creating plot function for x against y at each timestep

--- projectile_package/projectile_plot.py
+++ projectile_package/projectile_plot.py
@@ -1,1 +1,8 @@
-#adding file
+
+def plot_xy:
+
+    plt.plot(x_coord, height) 
+    plt.xlabel('x Coordinate (m)') 
+    plt.ylabel('Height (m)') 
+    plt.title('Height vs x Coordinate')
+    plt.show()
