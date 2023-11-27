44a255806747a1bb1741d8ba40fa3ce9f2967c9a
Tomas Williams <tomasjwilliams@icloud.com>
2023-11-27 20:05:47 +0000

    trying to define plt

--- projectile_package/projectile_plot.py
+++ projectile_package/projectile_plot.py
@@ -1,6 +1,10 @@
 
-def plot_xy():
 
+
+def plot_xy(x_coord, height):
+    
+    import matplotlib.pyplot as plt
+    
     plt.plot(x_coord, height) 
     plt.xlabel('x Coordinate (m)') 
     plt.ylabel('Height (m)') 
