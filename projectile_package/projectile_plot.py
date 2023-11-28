af4f4d8e116ac44a561e3aaedb947da4a9b3fcae
Tomas Williams <tomasjwilliams@icloud.com>
2023-11-28 17:20:11 +0000

    added comments to the plot_xy function

--- projectile_package/projectile_plot.py
+++ projectile_package/projectile_plot.py
@@ -1,12 +1,14 @@
 
 
-
+# defining function that plots x against y through time
 def plot_xy(x_coord, height):
     
     import matplotlib.pyplot as plt
     
+    # this plots a line graph showing the trajectory of the mass
     plt.plot(x_coord, height) 
     plt.xlabel('x Coordinate (m)') 
     plt.ylabel('Height (m)') 
     plt.title('Height vs x Coordinate')
+    # shows the figure
     plt.show()
